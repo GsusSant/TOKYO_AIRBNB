8133a87d7fc93f8a6eaf579ce2d777f0e4b62ddb
Jesús <161477129+GsusSant@users.noreply.github.com>
2024-04-18 16:48:00 +0200

    Update app.py

--- TOKYO_APP/app.py
+++ TOKYO_APP/app.py
@@ -61,9 +61,8 @@ im2='img\kgeisha.png'
 im='img\ksamu.png'
 nube = 'img\wordcloud.png'
 # --------------------COSAS QUE VAMOS A USAR EN TODA LA APP----------------------------#
-@st.cache_data()
 def load_data():
-    df = pd.read_csv("TOKYO_AIRBNB\TOKYO_APP\data\dairbnb_tokyo.csv")
+    df = pd.read_csv("TOKYO_AIRBNB\TOKYO_APP\data\dairbnb_tokyo.csv") 
     return df
 
 # --------------------CARGA DATOS----------------------------#
